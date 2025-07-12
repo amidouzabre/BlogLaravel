6ebf6922ad9c70e72fdc4aa00216f9e31d314dfd
amidou.zabre <amidou.zabre@gmail.com>
2025-07-12 14:56:47 +0000

    Controllers->PostController: create create() function

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
+use Inertia\Response;
 
 class PostController extends Controller
 {
-    //
+    public function create(): Response
+    {
+        if(!Auth::check()) {
+            abort(403);
+        }
+
+        return Inertia::render('Post/Create');
+    }
 }
