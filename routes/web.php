fb29b0f4323c741978d573082cd19bbfd7d7d0d9
amidou.zabre <amidou.zabre@gmail.com>
2025-07-12 15:52:41 +0000

    Routes->web: create posts routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -7,10 +9,23 @@ Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
 
+Route::middleware(['auth', 'verified'])->group(function(){
+    Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+    Route::post('/posts/store', [PostController::class, 'store'])->name('posts.store');
+    Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+    Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
+    Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
+    Route::post('/posts/{post}/like', [PostController::class, 'like'])->name('posts.like');
+});
+
+Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
-    })->name('dashboard');
+        return Inertia::render('dashboard',[
+            'usersPosts' => Auth::user()->posts()->with('author')->latest()->get(),
+        ]);
+    })->middleware(['auth', 'verified'])->name('dashboard');
 });
 
 require __DIR__.'/settings.php';
