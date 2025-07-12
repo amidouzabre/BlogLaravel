e821d53f2b9323bc5d2f69cdb70dd79062a0ceb4
amidou.zabre <amidou.zabre@gmail.com>
2025-07-12 21:55:52 +0000

    Types: create PostFormData interfaces

---
+++ resources/js/types/post.ts
@@ -0,0 +1,26 @@
+import {PageProps} from "@/types";
+
+
+export interface Author {
+    id: number;
+    name: string;
+}
+
+export interface Post {
+    id: number;
+    title: string;
+    description: string;
+    image: string | null;
+    createdAt: string;
+    author: Author;
+    is_liked: boolean;
+    likes_count: number;
+    user_id: number;
+}
+
+export interface PostFormData {
+    [key: string]: string | File | null;
+    title: string;
+    description: string;
+    image: File | null;
+}
