6b58fcccc306df8a0c08ac2f30640eb217a0f287
amidou.zabre <amidou.zabre@gmail.com>
2025-07-14 21:21:14 +0000

    Complete Nav

--- resources/js/components/Nav.tsx
+++ resources/js/components/Nav.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { Link, usePage } from '@inertiajs/react';
 
 export default function Nav() {
+  const { auth } = usePage().props as any;
   return (
-    <div>Nav  dsvdscvds </div>
+    <nav className='bg-white shadow-md border-b py-2'>
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between h-16">
+          <div className="flex items-center">
+            <Link href="/" className='text-2xl font-black text-indigo-600'>
+              MyApp
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
   )
 }
